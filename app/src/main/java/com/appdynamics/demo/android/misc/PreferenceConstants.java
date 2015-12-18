47d415f05b3075d3451120bff86d5303d15f2b79
Mark Prichard <mqprichard@gmail.com>
2015-12-18 15:45:06 -0800

    Renamed packages; added android.permission.ACCESS_NETWORK_STATE

---
+++ app/src/main/java/com/appdynamics/demo/android/misc/PreferenceConstants.java
@@ -0,0 +1,7 @@
+package com.appdynamics.demo.android.misc;
+
+public final class PreferenceConstants {
+    public final static String END_POINT_URL = "";
+    public static final String EUM_APP_KEY = "";
+    public static final String EUM_COLLECTOR_URL = "";
+}
--- app/src/main/java/com/appdynamics/pmdemoapps/android/ECommerceAndroid/misc/PreferenceConstants.java
+++
@@ -1,7 +0,0 @@
-package com.appdynamics.pmdemoapps.android.ECommerceAndroid.misc;
-
-public final class PreferenceConstants {
-    public final static String END_POINT_URL = ""; // Application URL
-    public static final String EUM_APP_KEY = ""; // EUM App Key
-    public static final String EUM_COLLECTOR_URL = ""; // EUM Collector URL
-}
